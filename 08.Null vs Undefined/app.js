2c573cbc17759081636e6cd9c5778a81bbb14a87
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-06-15 09:51:14 +0530

    added null vs defined in JavaScript

---
+++ 08.Null vs Undefined/app.js
@@ -0,0 +1,15 @@
+// Null and Undefined
+// Both represents no value
+
+// Undefined - "JavaScript cannot find value for this"
+// variable without value
+// missig function arguments
+// missing object properties
+
+// Null - "developer sets the value"
+
+let number1 = 20 + null;
+console.log(number1);
+
+let number2 = 20 + undefined;
+console.log(number2);
