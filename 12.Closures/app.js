359e06b1b2c197ae3284566da21034a128df52c3
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-06-25 00:05:32 +0530

    added closures in JS

---
+++ 12.Closures/app.js
@@ -0,0 +1,38 @@
+/**
+ * Closure = when a function remembers and uses variables from the outer scope even after that scope has finished running.
+ * JavaScript variables can belongs to the local or global scope
+ * Global variables can be made local (private) with closures.
+ * Nested functions are required to understand closures.
+ * A closure is a function having access to the parent scope, even after the parent function has closed
+ */
+function outer() {
+  let counter = 0;
+
+  function increment() {
+    counter++;
+    console.log(counter);
+  }
+
+  return increment;
+}
+
+const addOne = outer();
+
+addOne();
+addOne();
+addOne();
+
+function createCounter() {
+  let count = 0;
+
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const counter = createCounter();
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
