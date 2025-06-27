4f8940fe7f2e29d8b69e47d98ba9c8acefdb8041
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-06-27 10:52:01 +0530

    added namaste nodejs season-1 JS code

---
+++ 18.Namaste JS S-1/1.undefined vs notdefined.js
@@ -0,0 +1,15 @@
+console.log(a);
+// undefined is allocated memory but its value is undefined by JS
+
+var a = 7;
+
+console.log(a);
+
+console.log(x);
+// not defines is not allocated memory
+
+if (a === undefined) {
+  console.log("a is undefined");
+} else {
+  console.log("a is not undefined");
+}
