bfb981b07e5f25e2859833c590c95a14d9b01726
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-02 10:17:35 +0530

    added closures in JS

--- 12.Closures/app.js
+++ 12.Closures/app.js
@@ -5,34 +5,60 @@
  * Nested functions are required to understand closures.
  * A closure is a function having access to the parent scope, even after the parent function has closed
  */
-function outer() {
-  let counter = 0;
+// function outer() {
+//   let counter = 0;
 
-  function increment() {
-    counter++;
-    console.log(counter);
+//   function increment() {
+//     counter++;
+//     console.log(counter);
+//   }
+
+//   return increment;
+// }
+
+// const addOne = outer();
+
+// addOne();
+// addOne();
+// addOne();
+
+// function createCounter() {
+//   let count = 0;
+
+//   return function () {
+//     count++;
+//     return count;
+//   };
+// }
+
+// const counter = createCounter();
+
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+// function outer() {
+//   const outerVar = "Hey I am the outer Var";
+//   function inner() {
+//     const innerVar = "Hey I am an inner var";
+//     console.log(innerVar);
+//     console.log(outerVar);
+//   }
+//   return inner;
+// }
+
+// const innerFn = outer();
+// innerFn();
+
+// Counter example
+function counter() {
+  var count = 0;
+  console.log("Outer Function Executed");
+
+  function innerCount() {
+    console.log("Inner Function Executed");
+    count = count + 1;
+    console.log(count);
   }
-
-  return increment;
 }
-
-const addOne = outer();
-
-addOne();
-addOne();
-addOne();
-
-function createCounter() {
-  let count = 0;
-
-  return function () {
-    count++;
-    return count;
-  };
-}
-
-const counter = createCounter();
-
-console.log(counter());
-console.log(counter());
-console.log(counter());
+counter();
