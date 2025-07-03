a20ab1a6ece90c2cfc03b7b64dd138fa6122d606
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-03 13:07:06 +0530

    added comparison operator in JS

---
+++ 25.Expressions & Operators/comparisonOperator.js
@@ -0,0 +1,28 @@
+/**
+ *Comparison operators are used to compare two values and return either:
+
+true (✅ condition is met)
+
+false (❌ condition is not met)
+
+These are core to logic, conditions, and if-statements in JavaScript.
+
+
+| Operator | Name                  | Example     | Returns   |
+| -------- | --------------------- | ----------- | --------- |
+| `==`     | Equal to (loose)      | `5 == "5"`  | ✅ `true`  |
+| `===`    | Strict equal          | `5 === "5"` | ❌ `false` |
+| `!=`     | Not equal (loose)     | `5 != "5"`  | ❌ `false` |
+| `!==`    | Strict not equal      | `5 !== "5"` | ✅ `true`  |
+| `>`      | Greater than          | `7 > 5`     | ✅ `true`  |
+| `<`      | Less than             | `3 < 5`     | ✅ `true`  |
+| `>=`     | Greater than or equal | `6 >= 6`    | ✅ `true`  |
+| `<=`     | Less than or equal    | `2 <= 3`    | ✅ `true`  |
+
+
+✅ Used in:
+if statements
+loops
+conditions in filtering, sorting
+React conditional rendering (isLoggedIn === true ? ...)
+ */
