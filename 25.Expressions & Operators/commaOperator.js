eacc127c4cd58722f1d9895582f950018bb33fac
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-03 13:06:34 +0530

    added comma operator in JS

---
+++ 25.Expressions & Operators/commaOperator.js
@@ -0,0 +1,11 @@
+/**
+ * The comma operator (,) evaluates each of its operands (from left to right) and returns the value of the last operand. This lets you create a compound expression in which multiple expressions are evaluated, with the compound expression's final value being the value of the rightmost of its member expressions. This is commonly used to provide multiple parameters to a for loop.
+ */
+
+let x = 1;
+x = (x++, x);
+
+console.log(x);
+
+x = (4, 3);
+console.log(x);
