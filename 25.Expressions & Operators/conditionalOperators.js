bb3f8e158e11a034d68a9d8ca2768dd57328c560
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-04 00:00:18 +0530

    added conditional operator code in JS

---
+++ 25.Expressions & Operators/conditionalOperators.js
@@ -0,0 +1,7 @@
+let age = 23;
+let voteableAge = age < 18 ? "Too young" : "Old enough";
+console.log(voteableAge);
+
+let isLoggedIn = false;
+let subscribe = isLoggedIn === true ? "LogIn" : "Logout";
+console.log(subscribe);
