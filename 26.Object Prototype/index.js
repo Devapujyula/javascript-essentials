b2d3ac90b093e7a47745b3910fba6ca72ab471be
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-14 09:48:46 +0530

    added object protype in JS

---
+++ 26.Object Prototype/index.js
@@ -0,0 +1,11 @@
+let arr = ["Prakash", "Sunny", "Sagar"];
+
+let object = {
+  name: "Prakash",
+  city: "Amalapuram",
+  getIntro: function () {
+    console.log(this.name + " from " + this.city);
+  },
+};
+
+arr.__proto__;
