6a4a41d75f17555bae94e157f144b973cff80ae1
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-15 08:34:11 +0530

    added DOm in JS

---
+++ 28.DOM/app.js
@@ -0,0 +1,5 @@
+const listItems = document.querySelector("ul li:nth-child(4)");
+console.log(listItems);
+console.log(listItems.previousElement);
+console.log(listItems.nextSibling);
+console.log(listItems.nextSibling.nextSibling);
---
+++ 28.DOM/children&nodes.js
@@ -0,0 +1,8 @@
+const list = document.querySelector("ul");
+console.log(list); // How to know what are the childrens available for ul tag
+
+console.log(list.childNodes);
+console.log(list.children);
+
+const body = document.querySelector("body");
+console.log(body.children);
