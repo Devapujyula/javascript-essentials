5e0815e745ebb745d3996f54c5def5c3b3d4489d
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-18 08:53:32 +0530

    added events in JS

---
+++ 29.EventListeners/event.js
@@ -0,0 +1,10 @@
+/**
+ * Event listeners
+ * JavaScript offers three ways to add an Event Listener to a DOM element.
+ * Inline event listeners
+ * onevent listeners
+ * addEventListener()
+ */
+
+// Inline event listeners
+<button onClick="greetings()">Greet</button>;
