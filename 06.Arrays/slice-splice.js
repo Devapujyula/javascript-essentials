828f1402a364e1cc94d7b94c8f5f6f66696af6fa
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2025-07-24 10:58:21 +0530

    added slice and splice in JS code

---
+++ 06.Arrays/slice-splice.js
@@ -0,0 +1,16 @@
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+console.log(users);
+console.log(users.slice(1));
+
+users.splice(1, 2, "Hii");
+console.log(users);
+
+const array = [1, 2, 3, 4, 5, 6];
+console.log(array);
+array.unshift(0);
+console.log(array);
+array.shift();
+console.log(array);
+
+const arr = [, , ,];
+console.log(arr.length);
